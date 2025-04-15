18339b4f1d2236f06bc821c33d53b4c367689553
Luan Ferreira <luanhsouzaf@gmail.com>
2025-04-15 10:29:58 -0300

    Incluindo arquivos package.json e package-lock.json para facilitar a instalação

--- nestjs-crud-app-2/src/categories/dto/update-category.dto.ts
+++
@@ -1,4 +0,0 @@
-export class UpdateCategoryDto {
-  name?: string;
-  description?: string;
-}
--- nestjs-crud-app-2/src/products/dto/create-product.dto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateProductDto {
-  name!: string; // Adicionado o operador "!"
-  description!: string; // Adicionado o operador "!"
-  price!: number; // Adicionado o operador "!"
-  categoryId!: number; // Adicionado o operador "!"
-}
